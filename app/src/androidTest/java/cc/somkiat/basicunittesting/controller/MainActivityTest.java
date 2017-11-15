855d3c2b9dff24282910e64697b454e19fde46b8
tiwipabmin <tiwipabmin@gmail.com>
2017-11-16 01:54:07 +0700

    finished.

--- app/src/androidTest/java/cc/somkiat/basicunittesting/controller/MainActivityTest.java
+++ app/src/androidTest/java/cc/somkiat/basicunittesting/controller/MainActivityTest.java
@@ -1,24 +1,37 @@
 package cc.somkiat.basicunittesting.controller;
 
 
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+import cc.somkiat.basicunittesting.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import cc.somkiat.basicunittesting.R;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -28,21 +41,35 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void saveSuccessful() {
-        closeSoftKeyboard();
-        onView(withId(R.id.userNameInput)).perform(replaceText("tiwipabmin"));
-        onView(withId(R.id.emailInput)).perform(replaceText("tiwipabmin@gmail.com"));
-        onView(withId(R.id.saveButton)).perform(click());
-        onView(withText("Save successful")).check(matches(isDisplayed()));
+    public void mainActivityTest() {
+        ViewInteraction editText = onView(
+allOf(withId(R.id.userNameInput), withText("Enter your name"),
+childAtPosition(
+childAtPosition(
+IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+0),
+2),
+isDisplayed()));
+        editText.check(matches(withText("Enter your name")));
+        
+        }
 
+        private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup)parent).getChildAt(position));
+            }
+        };
     }
-
-    @Test
-    public void revertTest() {
-        closeSoftKeyboard();
-        onView(withId(R.id.revertButton)).perform(click());
-        onView(withId(R.id.userNameInput)).check(matches(withText("")));
-        onView(withId(R.id.emailInput)).check(matches(withText("")));
     }
-
-}
